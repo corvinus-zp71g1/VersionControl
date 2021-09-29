c3ec0c07a43827efcbc55b1c66d68fb156d9673d
corvinus-zp71g1 <mate.dravecz@stud.uni-corvinus.hu>
2021-09-29 17:08:20 +0200

    tetszőleges5

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace UserMaintenance
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.txtFirstName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -80,7 +81,7 @@ namespace UserMaintenance
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(468, 217);
+            this.button1.Location = new System.Drawing.Point(468, 178);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(207, 74);
             this.button1.TabIndex = 5;
@@ -88,11 +89,22 @@ namespace UserMaintenance
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(468, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 73);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.txtLastName);
@@ -114,6 +126,7 @@ namespace UserMaintenance
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.TextBox txtFirstName;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,7 @@ namespace UserMaintenance
             InitializeComponent();
 
             label1.Text = Resource1.FullName; // label1
-            label2.Text = Resource1.FirstName; // label2
+            //label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
 
             listUsers.DataSource = users;
@@ -35,6 +36,22 @@ namespace UserMaintenance
                 FullName = txtLastName.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var s in users)
+                {
+                    sw.Write(s.FullName);
+                    sw.Write(";");
+                    sw.Write(s.ID);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
