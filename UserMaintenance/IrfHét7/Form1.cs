185e1497196bddf6fc5153e97b9f55ea4748779b
corvinus-zp71g1 <mate.dravecz@stud.uni-corvinus.hu>
2021-10-26 17:52:21 +0200

    4es vége

--- UserMaintenance/IrfHét7/Form1.cs
+++ UserMaintenance/IrfHét7/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IrfHét7.Entities;
+using IrfHét7.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,31 @@ namespace IrfHét7
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+
+            dataGridView1.DataSource = Rates;
+            Füg1();
+
+        }
+
+        public void Füg1() 
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
