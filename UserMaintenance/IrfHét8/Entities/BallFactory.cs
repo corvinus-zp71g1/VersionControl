c0ea838ec8cbc41c37707abeb4d4201cdfc14f20
corvinus-zp71g1 <mate.dravecz@stud.uni-corvinus.hu>
2021-11-03 19:41:53 +0100

    nagyja működik

--- UserMaintenance/IrfHét8/Entities/BallFactory.cs
+++ UserMaintenance/IrfHét8/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IrfHét8.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace IrfHét8.Entities
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
--- UserMaintenance/IrfHét8/Entities/CarFactory.cs
+++ UserMaintenance/IrfHét8/Entities/CarFactory.cs
@@ -1,19 +1,17 @@
 ﻿using IrfHét8.Abstractions;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace IrfHét8.Entities
 {
-    public class Ball : Toy
+    public class CarFactory : IToyFactory
     {
-        protected override void DrawImage(Graphics g)
+        public Toy CreateNew()
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            return new Car();
         }
     }
 }
