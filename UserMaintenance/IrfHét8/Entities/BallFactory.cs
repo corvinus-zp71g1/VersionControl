c094ff54f549300fa9694554eca96e9d2e8c4f26
corvinus-zp71g1 <mate.dravecz@stud.uni-corvinus.hu>
2021-11-03 19:05:46 +0100

    mentesnek

---
+++ UserMaintenance/IrfHét8/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrfHét8.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
