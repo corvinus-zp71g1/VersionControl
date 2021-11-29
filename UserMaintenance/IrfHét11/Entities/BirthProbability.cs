5b45b0a3a33c9d23f5b355ff1351b947a3ebf0c5
corvinus-zp71g1 <mate.dravecz@stud.uni-corvinus.hu>
2021-11-29 12:10:04 +0100

    classes done

---
+++ UserMaintenance/IrfHét11/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrfHét11.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int GyerekSzam { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ UserMaintenance/IrfHét11/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrfHét11.Entities
+{
+    public  class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Kor { get; set; }
+        public double P { get; set; }
+    }
+}
