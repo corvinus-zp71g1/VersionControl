4f1bffd9126ea025d28631bb3c191a94becf1ba6
corvinus-zp71g1 <mate.dravecz@stud.uni-corvinus.hu>
2021-12-06 12:19:03 +0100

    first generacio

--- UserMaintenance/IrfHét12/Form1.cs
+++ UserMaintenance/IrfHét12/Form1.cs
@@ -16,6 +16,11 @@ namespace IrfHét12
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,8 +28,28 @@ namespace IrfHét12
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+
+            };
+
+            gc.GameOver += Gc_GameOver;
+            gc.Start();
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
